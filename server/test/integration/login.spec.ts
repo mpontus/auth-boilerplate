54c7608247724a1c206f0c46d8fe82522b8deb38
Mikhail Pontus <m.pontus@gmail.com>
2018-10-04 16:49:00 +0300

    Test login

---
+++ server/test/integration/login.spec.ts
@@ -0,0 +1,49 @@
+import * as request from 'supertest';
+import { resetDb } from '../utils/resetDb';
+import { initApp } from '../utils/initApp';
+
+let expressApp, nestApp;
+
+beforeAll(async () => {
+  ({ expressApp, nestApp } = await initApp());
+});
+
+afterAll(() => nestApp.close());
+
+beforeEach(() => resetDb());
+
+describe('signup', () => {
+  describe('when credentials are correct', () => {
+    const seed = require('../seeds/registered_user');
+
+    beforeEach(() => seed.run());
+
+    it('should be successful', async () => {
+      const response = await request(expressApp)
+        .post('/auth/login')
+        .send({
+          email: seed.email,
+          password: seed.password,
+        })
+        .expect(201);
+
+      expect(response.body).toMatchSnapshot({
+        token: expect.any(String),
+      });
+    });
+  });
+
+  describe('when the user does not exist', () => {
+    it('should be an error', async () => {
+      const response = await request(expressApp)
+        .post('/profile')
+        .send({
+          email: 'nfisher@yahoo.com',
+          password: '9O_8ywUKpHuHjnZ',
+        })
+        .expect(400);
+
+      expect(response.body).toMatchSnapshot();
+    });
+  });
+});
--- server/test/seeds/registered_user.ts
+++ server/test/seeds/registered_user.ts
@@ -1,10 +1,11 @@
+import * as bcrypt from 'bcrypt';
 import { getConnection } from 'typeorm';
 import { UserEntity } from '../../src/module/user/user.entity';
 
 export const id = 123;
 export const email = 'Toby38@gmail.com';
-export const passwordHash =
-  '$2b$06$S5uyAfnGridPcLUdsR.Uq.QkZvg.4hPtgUPPYZLaqk.Pn5YzF5KTm';
+export const password = 'H$4Dxli4R8';
+export const passwordHash = bcrypt.hashSync(password, 6);
 
 export const run = () =>
   getConnection().manager.insert(UserEntity, {

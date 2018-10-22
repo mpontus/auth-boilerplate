d79f8da76cc56eae3470b349eff219d94956ac4e
Mikhail Pontus <m.pontus@gmail.com>
2018-10-22 19:05:21 +0300

    Expose additional fields on serialized User

--- server/src/user/transport/serializer/User.ts
+++ server/src/user/transport/serializer/User.ts
@@ -2,6 +2,12 @@ import { Exclude, Expose } from 'class-transformer';
 
 /**
  * User serializer
+ *
+ * Provides extensive details about the user, which should only be
+ * visible to the user themself and admins.
+ *
+ * Front-end needs are taken into consideration when deciding which of
+ * the user fields to expose.
  */
 @Exclude()
 export class User {
@@ -12,8 +18,26 @@ export class User {
   public id: string;
 
   /**
-   * Expose user name
+   * Expose user display name to display session status
    */
   @Expose()
   public name: string;
+
+  /**
+   * Expose user email for profile updates
+   */
+  @Expose()
+  public email: string;
+
+  /**
+   * Expose whether email is verified to display a reminder
+   */
+  @Expose()
+  public emailVerified: boolean;
+
+  /**
+   * Expose user roles to constrain navigation options
+   */
+  @Expose()
+  public roles: string[];
 }
